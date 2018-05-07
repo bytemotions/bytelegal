805f74cbf809213e1630c123017081f33b7d0095
Juan Facundo Cena <jfcena@gmail.com>
2018-05-08 00:23:40 +0200

    single.php > added customized comment_form

--- comments.php
+++ comments.php
@@ -40,8 +40,7 @@ if ( post_password_required() )
             ?>
         </ol><!-- .comment-list -->
  
-        <?php
-            // Are there comments to navigate through?
+        <?php  // Are there comments to navigate through?
             if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :
         ?>
         <nav class="navigation comment-navigation" role="navigation">
@@ -58,7 +57,79 @@ if ( post_password_required() )
     <?php endif; // have_comments() ?>
  
 	<!-- Comments Form Starts -->
-        <?php comment_form(); ?>
+    <?php 
+
+        $commenter = wp_get_current_commenter();
+        $req = get_option( 'require_name_email' );
+        $aria_req = ( $req ? " aria-required='true'" : '' );
+                
+        $fields =  array(
+
+            'author' =>
+                '<p class="comment-form-author"><label for="author">' . __( 'Name', 'domainreference' ) .
+                ( $req ? '<span class="required">*</span>' : '' ) . '</label>' .
+                '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
+                '" size="30"' . $aria_req . ' /></p>',
+          
+            'email' =>
+                '<p class="comment-form-email"><label for="email">' . __( 'Email', 'domainreference' ) .
+                ( $req ? '<span class="required">*</span>' : '' ) . '</label>' .
+                '<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+                '" size="30"' . $aria_req . ' /></p>',
+          
+            'url' =>
+                '<p class="comment-form-url"><label for="url">' . __( 'Website', 'domainreference' ) . '</label>' .
+                '<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
+                '" size="30" /></p>',
+            );
+
+            $args = array(
+                'id_form'           => 'commentform',
+                'class_form'      => 'comment-form',
+                'id_submit'         => 'submit',
+                'class_submit'      => 'submit',
+                'name_submit'       => 'submit',
+                'title_reply'       => __( 'Leave a Reply' ),
+                'title_reply_to'    => __( 'Leave a Reply to %s' ),
+                'cancel_reply_link' => __( 'Cancel Reply' ),
+                'label_submit'      => __( 'Post Comment' ),
+                'format'            => 'xhtml',
+              
+                'comment_field' =>  '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) .
+                  '</label><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true">' .
+                  '</textarea></p>',
+              
+                'must_log_in' => '<p class="must-log-in">' .
+                  sprintf(
+                    __( 'You must be <a href="%s">logged in</a> to post a comment.' ),
+                    wp_login_url( apply_filters( 'the_permalink', get_permalink() ) )
+                  ) . '</p>',
+              
+                'logged_in_as' => '<p class="logged-in-as">' .
+                  sprintf(
+                  __( 'Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Log out?</a>' ),
+                    admin_url( 'profile.php' ),
+                    $user_identity,
+                    wp_logout_url( apply_filters( 'the_permalink', get_permalink( ) ) )
+                  ) . '</p>',
+              
+                'comment_notes_before' => '<p class="comment-notes">' .
+                  __( 'Your email address will not be published.' ) . ( $req ? $required_text : '' ) .
+                  '</p>',
+              
+                'comment_notes_after' => '<p class="form-allowed-tags">' .
+                  sprintf(
+                    __( 'You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes: %s' ),
+                    ' <code>' . allowed_tags() . '</code>'
+                  ) . '</p>',
+              
+                'fields' => apply_filters( 'comment_form_default_fields', $fields ),
+              );
+              
+
+        comment_form($args);
+
+    ?>
     <!-- Comments Form Ends -->
  
 </div><!-- #comments -->
